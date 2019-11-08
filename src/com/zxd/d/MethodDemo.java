7adc75cc159ea588bd84ffd83d925980a3fbd6fc
zxd3333 <1274752060@qq.com>
2019-11-08 12:30:57 +0800

    添加了静态方法的调用

--- src/com/zxd/d/MethodDemo.java
+++ src/com/zxd/d/MethodDemo.java
@@ -3,11 +3,18 @@ package com.zxd.d;
 //方法
 public class MethodDemo {
 
+    private int a = 100;
+    private static int b =50;
+
     public static void main(String[] args) {
-        //调用非静态方法
-        //首先需要本类的对象
+        //静态方法调用非静态成员变量和方法
+        //首先需要实例化类
         MethodDemo d = new MethodDemo();
-        //通过对象名.方法名(参数)进行调用
+        //通过对象名.变量名调用成员变量
+        System.out.println(d.a);
+        //直接调用静态变量
+        System.out.println(b);
+        //通过对象名.方法名(参数)调用成员方法
         d.method1();
 
         int m = d.method2();
@@ -19,7 +26,11 @@ public class MethodDemo {
         int n = d.method3(10,20);
         System.out.println("返回值为：" + n);
 
-        //调用静态方法
+        //第五个方法
+        //研究的是普通方法调用静态变量和方法
+        d.method4();
+
+        //直接调用静态方法
         method();
     }
 
@@ -36,6 +47,12 @@ public class MethodDemo {
     public int method3(int a, int b){
         System.out.println("这个方法： 带参  有返回值");
         return a > b ? a : b;
+    }
+    public void method4(){
+        System.out.println(a);
+        System.out.println(b);
+        method1();
+        method();
     }
 
     //静态方法
