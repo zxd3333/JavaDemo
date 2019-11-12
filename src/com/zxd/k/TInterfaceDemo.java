e7041961f8e83f4af01e13cfc0288d1a367eae5d
zxd3333 <1274752060@qq.com>
2019-11-12 22:27:52 +0800

    不太了解的泛型

---
+++ src/com/zxd/k/TInterfaceDemo.java
@@ -0,0 +1,6 @@
+package com.zxd.k;
+
+//泛型接口
+public interface TInterfaceDemo<T> {
+    T show();
+}
---
+++ src/com/zxd/k/TInterfaceTest.java
@@ -0,0 +1,12 @@
+package com.zxd.k;
+
+//泛型接口测试类
+public class TInterfaceTest {
+
+    public static void main(String[] args) {
+
+        //根据实现类中的类型来确定这里的类型
+        TInterfaceDemo<Integer> t = new TInterfaceImpl();
+        System.out.println(t.show());
+    }
+}
