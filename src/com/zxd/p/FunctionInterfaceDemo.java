189b303ca44df9384117be20ecbae94486f68379
zxd3333 <1274752060@qq.com>
2019-11-26 14:13:13 +0800

    函数式接口初步

--- src/com/zxd/p/FunctionInterfaceDemo.java
+++ src/com/zxd/p/FunctionInterfaceDemo.java
@@ -1,16 +1,22 @@
 package com.zxd.p;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 //函数式接口
 //Java中函数式编程体现的就是Lambda表达式
 public class FunctionInterfaceDemo {
 
     public static void main(String[] args) {
-        //函数式接口的使用
-        //Lambda表达式表示该接口的一个实现
+        //函数式接口的初步使用
+        //接口不能实例化，可以使用Lambda表达式表示该接口的一个实现
         MyFunctionInterface mfi = ()-> System.out.println(123);
         mfi.show();
 
-        //匿名内部类调用函数式接口
+        //函数式接口作为方法的参数
+        //如果一个方法的参数是一个函数式接口，我们可以把Lambda表达式作为参数传递
+        //正常情况下，使用匿名内部类调用函数式接口
         method1(new Runnable() {
             @Override
             public void run() {
@@ -18,8 +24,22 @@ public class FunctionInterfaceDemo {
             }
         });
 
-        //Lambda表达式调用
+        //与之对比，采用Lambda表达式作为参数传递
         method1(()-> System.out.println("Lambda表达式调用"));
+
+        //函数式接口作为方法的返回值
+        //当一个方法的返回值是一个函数式接口，可以把Lambda表达式作为结果返回
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("a");
+        arrayList.add("bb");
+        arrayList.add("dd");
+        arrayList.add("cccc");
+
+        System.out.println("按录入的顺序输出：" + arrayList);
+        Collections.sort(arrayList);
+        System.out.println("按照A~Z的顺序输出：" + arrayList);
+        Collections.sort(arrayList,method2());
+        System.out.println("按照自定义的顺序(字符串的长度)输出：" + arrayList);
 
     }
 
@@ -28,5 +48,13 @@ public class FunctionInterfaceDemo {
     public static void method1(Runnable runnable){
         new Thread(runnable).start();
     }
+
+    //查看源码发现Comparator<T> 是一个函数式接口
+    //作用是对数组进行排序
+    //当一个方法的返回值是一个函数式接口，可以把Lambda表达式作为结果返回
+    public static Comparator<String> method2(){
+        return (o1,o2)->o1.length() - o2.length();
+    }
+
 }
 
