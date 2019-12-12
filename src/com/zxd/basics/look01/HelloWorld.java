c6c737c5ce00d381f51440dea468aea666b1de7c
zxd3333 <1274752060@qq.com>
2019-12-12 12:57:51 +0800

    简单了解 + HelloWorld

---
+++ src/com/zxd/basics/look01/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.zxd.basics.look01;
+
+//第一个程序 HelloWorld
+public class HelloWorld {
+    /*
+        main方法是程序的入口
+        代码的执行是从main方法开始的
+    */
+    public static void main(String[] args) {
+        // 输出语句
+        System.out.println("Hello,World.");
+    }
+}
